99ed6b3b1ef4a47c21fc41129f4d357b7bc8fd70
aleksey-ruban <alekseyruban555@gmail.com>
2024-04-24 00:35:23 +0300

    first commit

---
+++ src/test/java/com/example/dockermaven/DockerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
